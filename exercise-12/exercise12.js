67fcacf0070992a433c6d92ab1fe6d1212f70bb1
roudabehadnani <rodabeh.adnani@gmail.com>
2022-11-06 12:18:00 +0100

    exercise 12

---
+++ exercise-12/exercise12.js
@@ -0,0 +1,22 @@
+
+function myFunction(){
+
+    for(let i=0; i<=15; i++){
+        if(i === 0){
+            document.write(i + " is even" + "<br>");
+            
+        }
+        else if(i % 2 === 0){
+             
+            document.write(i + " is even" + "<br>");
+            
+        }
+        else{
+            document.write(i + " is odd" + "<br>");
+          
+        }
+
+    }
+    
+}
+
